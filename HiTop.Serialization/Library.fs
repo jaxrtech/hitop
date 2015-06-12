d6d5f56ea09f32a9478b8647c9bcc8037041df21
Joshua Bowden <josbow@gmail.com>
2015-06-12 15:00:15 -0400

    Create serialization library project

---
+++ HiTop.Serialization/Library.fs
@@ -0,0 +1,10 @@
+﻿module HiTop.Serialization
+
+open System.IO
+
+let serializeTo (program: byte []) (stream: Stream) =
+    stream.Write(program, 0, program.Length)
+
+let derserializeFrom (stream: Stream) =
+    let result = Array.zeroCreate (stream.Length |> int)
+    stream.Read(result, 0, result.Length)
