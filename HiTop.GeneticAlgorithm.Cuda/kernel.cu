61e9c5d2a7c1eb3384233cb7d076c496fe5aa4e1
Joshua Bowden <josbow@gmail.com>
2015-06-23 01:39:55 -0400

    Fix compilation and runtime errors. Add tourny selection. Adds TODOs.
    Also remove x64 compilation target, since the CUDA runtime will break
    when you do this and error messages are meaningless.

--- HiTop.GeneticAlgorithm.Cuda/kernel.cu
+++ HiTop.GeneticAlgorithm.Cuda/kernel.cu
@@ -2,7 +2,6 @@
 #include <iomanip>
 #include <fstream>
 #include <sstream>
-#include <ctime>
 
 #include <thrust/device_new.h>
 #include <thrust/device_delete.h>
@@ -14,6 +13,18 @@
 #include <thrust/random.h>
 
 #include "util.h"
+
+// Monkey patch onto thrust
+namespace thrust {
+
+template<typename T>
+thrust::device_ptr<T> device_new(const T& host_ptr)
+{
+	return thrust::device_new<T>(thrust::device_malloc<T>(1),
+								 host_ptr);
+}
+
+}
 
 namespace hitop {
 namespace algo {
@@ -64,6 +75,15 @@ uint32_t hash64_32(uint64_t key)
 	return static_cast<uint32_t>(key);
 }
 
+__host__ __device__
+uint32_t get_entropy_with_ptr(const void* address)
+{
+	auto entropy = reinterpret_cast<uintptr_t>(address);
+	uint32_t seed = util::hash32(entropy) ^ util::hash32(clock());
+
+	return seed;
+}
+
 } // namespace util
 
 namespace program_descriptor {
@@ -81,25 +101,24 @@ struct ProgramDescriptor
 struct create_random : public thrust::unary_function<size_t, ProgramDescriptor>
 {
 private:
-	algo::Settings* settings;
-	thrust::device_ptr<algo::Settings> settings_ptr;
+	Settings* settings_;
+
+	//algo::util::RandomContext* random_context_;
 
 public:
 	create_random(thrust::device_ptr<algo::Settings> settings_ptr)
-		: settings_ptr(settings_ptr)
-		, settings(settings_ptr.get())
+		: settings_(settings_ptr.get())
 	{ }
 
 	__host__ __device__
 		ProgramDescriptor operator()(size_t index)
 	{
-		uint32_t seed = hitop::algo::util::hash64_32(index);
-
+		uint32_t seed = util::get_entropy_with_ptr(this);
 		thrust::default_random_engine rng(seed);
-		thrust::uniform_int_distribution<size_t> program_length(settings->min_program_size, settings->max_program_size);
+		thrust::uniform_int_distribution<size_t> program_length(settings_->min_program_size, settings_->max_program_size);
 
 		ProgramDescriptor descriptor;
-		descriptor.pos = settings->block_size * index;
+		descriptor.pos = settings_->block_size * index;
 		descriptor.length = program_length(rng);
 		descriptor.score = NAN;
 
@@ -126,6 +145,9 @@ struct score : public thrust::unary_function<ProgramDescriptor, score_t>
 };
 
 } // namespace program_descriptor
+
+// Import type into `algo` namespace
+typedef typename program_descriptor::score_t score_t;
 
 namespace selection_result {
 
@@ -177,31 +199,105 @@ struct from_index : public thrust::unary_function<size_t, SelectionResult>
 
 } // namespace selection_result
 
+namespace selection_method {
+
+typedef selection_result::SelectionResult SelectionResult;
+typedef program_descriptor::ProgramDescriptor ProgramDescriptor;
+
+struct tournament_selection : public thrust::unary_function<size_t, SelectionResult>
+{
+	struct Context
+	{
+		thrust::device_ptr<ProgramDescriptor> program_descriptors;
+		size_t program_descriptors_count;
+
+		uint32_t rounds;
+	};
+
+
+#define DEBUG_PRINT_LINE() printf("%d\n", __LINE__);
+
+private:
+	Context *context_;
+
+	thrust::default_random_engine rng_;
+	
+	typedef thrust::uniform_int_distribution<size_t> indices_range_distribution;
+	indices_range_distribution indices_range_;
+
+	__host__ __device__
+	size_t get_random_index()
+	{
+		return indices_range_(rng_);
+	}
+
+	__host__ __device__
+	size_t get_next_winner_index()
+	{
+		auto program_descriptors = context_->program_descriptors.get();
+
+		auto cur_idx = get_random_index();
+		auto cur_score = program_descriptors[cur_idx].score;
+
+		for (uint32_t i = 0; i < context_->rounds; i++) {
+			auto other_idx = get_random_index();
+			auto other_score = program_descriptors[other_idx].score;
+
+			if (other_score > cur_score) {
+				cur_idx = other_idx;
+			}
+		}
+
+		return cur_idx;
+	}
+
+
+public:
+	tournament_selection(thrust::device_ptr<Context> context_ptr)
+		: context_(context_ptr.get())
+	{
+		rng_ = thrust::default_random_engine(util::get_entropy_with_ptr(this));
+	}
+
+	__host__ __device__
+	SelectionResult operator()(size_t index)
+	{
+		// Only now can we dereference `context_` since we are now dereferencing a device pointer on
+		// the device versus in the constructor which is solely on the host.
+		indices_range_ = indices_range_distribution(0, context_->program_descriptors_count);
+
+		return selection_result::create_pair_indices(index, get_next_winner_index());
+	}
+};
+
+} // namespace selection_method
+
 namespace program {
 
 struct fill : public thrust::unary_function<program_descriptor::ProgramDescriptor&, void>
 {
 private:
-	thrust::device_ptr<uint8_t> pool;
+	thrust::device_ptr<uint8_t> pool_;
+
+	thrust::default_random_engine rng_;
 
 public:
 	fill(thrust::device_ptr<uint8_t> pool)
-		: pool(pool)
-	{ }
+		: pool_(pool)
+	{
+		rng_ = thrust::default_random_engine(util::get_entropy_with_ptr(this));
+	}
 
 	__host__ __device__
 		void operator()(program_descriptor::ProgramDescriptor& descriptor)
 	{
-		uint32_t seed = hitop::algo::util::hash64_32(descriptor.pos);
-
-		thrust::default_random_engine rng(seed);
 		thrust::uniform_int_distribution<uint8_t> byte_range(0, UINT8_MAX);
 
 		for (size_t i = descriptor.pos;
 			 i < descriptor.pos + descriptor.length;
 			 i++) {
 
-			pool[i] = byte_range(rng);
+			pool_[i] = byte_range(rng_);
 		}
 	}
 };
@@ -234,8 +330,10 @@ public:
 
 } // namespace program
 
-}
-}
+} // namespace algo
+} // namespace hitop
+
+
 
 int main(int argc, char* argv[])
 {
@@ -266,7 +364,9 @@ int main(int argc, char* argv[])
 
 	std::cout
 		<< "input file: '" << settings.input_path << std::endl
-		<< "size: " << target_length << std::endl;
+		<< "size: " << target_length
+		<< std::endl
+		<< std::endl;
 
 	// Wait for CUDA to initialize
 	std::cout
@@ -309,19 +409,20 @@ int main(int argc, char* argv[])
 	const bool enable_stats_output = true;
 	const size_t generations_per_stats_output = 1;
 
+	const size_t program_pool_size = population_count * target_length;
+	assert(program_pool_size > 0);
+
+	size_t generation_num = 0;
+
+	//
+
 	algo::Settings program_settings_h;
 	program_settings_h.min_program_size = target_length * (0.75);
 	program_settings_h.max_program_size = target_length;
 	program_settings_h.block_size = target_length;
 
-	thrust::device_ptr<algo::Settings> program_settings_d = thrust::device_new<algo::Settings>();
-	cudaMemcpy(program_settings_d.get(), &program_settings_h, sizeof(algo::Settings), cudaMemcpyHostToDevice);
+	auto program_settings_d = thrust::device_new<algo::Settings>(program_settings_h);
 
-	const size_t program_pool_size = population_count * target_length;
-	assert(program_pool_size > 0);
-	
-	size_t generation_num = 0;
-	
 	//
 	// Allocate device_vectors
 	//
@@ -374,6 +475,19 @@ int main(int argc, char* argv[])
 		<< "debug: done"
 		<< std::endl
 		<< std::endl;
+
+	//
+	// Setup selection settings now that we have allocated everything
+	//
+
+	typedef typename algo::selection_method::tournament_selection::Context SelectionContext;
+
+	SelectionContext selection_context_h;
+	selection_context_h.program_descriptors = program_descriptors.data();
+	selection_context_h.program_descriptors_count = program_descriptors.size();
+	selection_context_h.rounds = 5;
+
+	auto selection_context_d = thrust::device_new<SelectionContext>(selection_context_h);
 
 	//
 	// Initialize initial generation
@@ -434,16 +548,16 @@ int main(int argc, char* argv[])
 	if (enable_stats_output
 		&& generation_num % generations_per_stats_output == 0) {
 
-		ProgramDescriptor best = program_descriptors.front();
-		ProgramDescriptor worst = program_descriptors.back();
+		ProgramDescriptor best = *(program_descriptors.begin());
+		ProgramDescriptor worst = *(program_descriptors.end() - 1);
 
-		auto sum = thrust::transform_reduce(program_descriptors.begin(),
-					                        program_descriptors.end(),
-											program_descriptor::score(),
-								            0,
-								            thrust::plus<program_descriptor::score_t>());
+		score_t sum = thrust::transform_reduce(program_descriptors.begin(),
+					                           program_descriptors.end(),
+											   program_descriptor::score(),
+								               0.0f,
+								               thrust::plus<program_descriptor::score_t>());
 
-		auto avg = sum / program_descriptors.size();
+		score_t avg = sum / program_descriptors.size();
 
 		std::cout
 			<< "gen " << generation_num << ": "
@@ -462,7 +576,15 @@ int main(int argc, char* argv[])
 	// Select elites if setting specified
 	//
 
+	std::cout
+		<< "debug: selecting elites..."
+		<< std::endl;
+
+	// Keep track of the position to start at in the case that we use elites to skip running the
+	// selection method over some of the programs
 	auto selection_results_start = selection_results.begin();
+	auto program_descriptors_start = program_descriptors.begin();
+	auto start_index = 0;
 
 	if (selection_elites > 0) {
 		thrust::transform(thrust::counting_iterator<size_t>(0),
@@ -470,21 +592,68 @@ int main(int argc, char* argv[])
 						  selection_results_start,
 						  selection_result::from_index());
 
+		start_index += selection_elites;
 		thrust::advance(selection_results_start, selection_elites);
+		thrust::advance(program_descriptors_start, selection_elites);
 	}
+
+	std::cout
+		<< "debug: done"
+		<< std::endl
+		<< std::endl;
 
 	//
 	// Run the selection method on the rest
 	//
 
-	// TODO: Finish the rest of this
+	std::cout
+		<< "debug: selecting the rest of population with selection method..."
+		<< std::endl;
+
+	thrust::transform(thrust::counting_iterator<size_t>(start_index),
+					  thrust::counting_iterator<size_t>(program_descriptors.size()),
+					  selection_results_start,
+					  selection_method::tournament_selection(selection_context_d));
+
+	std::cout
+		<< "debug: done"
+		<< std::endl
+		<< std::endl;
+
+	//
+	// Crossover selection results
+	//
+
+	// TODO
+
+	//
+	// Mutate newly generated population
+	//
+
+	// TODO
+
+	//
+	// Swap newly created population with current population vector
+	//
+
+	// TOOD
+
+	//
+	// Loop back with new generation
+	//
+
+	// TODO
 
 	//
 	// End algo
 	//
 
 	// Clear any `device_ptr`s
-	thrust::device_delete(program_settings_d);
+	
+	// FIXME: Trying to delete the `thrust::device_ptr` is breaking the build
+	//thrust::device_delete(program_settings_d);
+	//thrust::device_delete(selection_context_d);
+	//thrust::device_delete(random_context_d);
 
 	std::cout
 		<< "info: algorithm ran to completion!" << std::endl
@@ -493,4 +662,3 @@ int main(int argc, char* argv[])
 	std::cin.ignore();
 	return 0;
 }
-
